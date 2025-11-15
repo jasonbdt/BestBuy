52140dd232e778863ff72cbf06ba2a85f7807660
jasonbdt <ja.bladt@gmail.com>
2025-11-15 01:36:14 +0100

    Remove decimals from product price at outputs

--- products.py
+++ products.py
@@ -103,7 +103,7 @@ class Product:
         Side Effects:
             Writes a formatted summary to stdout.
         """
-        print(f"{self.name}, Price: ${self.price:.2f}, Quantity: {self.quantity}")
+        print(f"{self.name}, Price: ${self.price}, Quantity: {self.quantity}")
 
 
     def buy(self: Self, quantity: int) -> float:
